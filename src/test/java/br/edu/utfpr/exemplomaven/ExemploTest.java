c3a4c83cc7643d2cae4c058b54923bd12fee4fd1
GabrielTakazaki <GabrielTakazaki@hotmail.com>
2018-09-12 22:36:49 -0300

    Arrumando Testes

--- src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
+++ src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
@@ -33,29 +33,44 @@ public class ExemploTest {
     
     @BeforeClass
     public static void beforeClass() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().proxy("10.20.10.50:3128").setup();
     }
     
     @Before
     public void before() {
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         chromeOptions.addArguments("headless");
         chromeOptions.addArguments("window-size=1200x600");
         chromeOptions.addArguments("start-maximized");
         
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        driver.get("https://ration.io/");
+        WebElement botaoLogin = driver.findElement(By.xpath("//*[@id=\"page-header\"]/div/a[2]"));
+        botaoLogin.click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"login\"]/div/h1")).
+                                                                    getText().toLowerCase().startsWith("sign in") );
+        WebElement campoEmail = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[1]/input"));
+        campoEmail.sendKeys("softwareutfpr@gmail.com");
+        WebElement campoSenha = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[2]/input"));
+        campoSenha.sendKeys("teste");
+        WebElement campoSignIn = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[4]/button"));
+        campoSignIn.click();
     }
     
     @After
     public void after() {
         driver.close();
     }
-    
+    /*
     @Test
     public void testGoogleSearch() {
         driver.get("https://www.google.com.br/");
         WebElement searchInput = driver.findElement(By.name("q"));
+        
         searchInput.sendKeys("teste de software");
         
         takeScreenShot();
@@ -68,6 +83,16 @@ public class ExemploTest {
         takeScreenShot();
         
         assertTrue(driver.getTitle().startsWith("teste de software"));
+    }
+    */
+    @Test
+    public void testeLogin(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"friends\"]/div/div[1]/h1")).
+                                                                    getText().toLowerCase().startsWith("friends") );
+        
+        
+        takeScreenShot();
     }
     
     private void takeScreenShot() {
