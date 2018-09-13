550c6ef17eda221aa78b5087638864d127bad4f2
José Ede de Campos Neto <jose.ede.neto@gmail.com>
2018-09-13 17:27:41 -0300

    teste Logado e teste Help

--- src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
+++ src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
@@ -33,13 +33,12 @@ public class ExemploTest {
     
     @BeforeClass
     public static void beforeClass() {
-        WebDriverManager.chromedriver().proxy("10.20.10.50:3128").setup();
+        WebDriverManager.chromedriver().setup();
     }
     
     @Before
     public void before() {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         chromeOptions.addArguments("headless");
         chromeOptions.addArguments("window-size=1200x600");
         chromeOptions.addArguments("start-maximized");
@@ -47,6 +46,59 @@ public class ExemploTest {
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         
+        fazLogin();
+    }
+    
+    @After
+    public void after() {
+        driver.close();
+    }
+
+    @Test
+    public void testeLogado(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"friends\"]/div/div[1]/h1")).
+                                                                    getText().toLowerCase().startsWith("friends") );
+        
+        takeScreenShot();
+    }
+
+    @Test
+    public void testeHelp(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"friends\"]/div/div[1]/h1")).
+                                                                    getText().toLowerCase().startsWith("friends") );
+        WebElement botaoHelp = driver.findElement(By.xpath("//*[@id=\"page-header\"]/div/a[3]"));
+        botaoHelp.click();
+        WebElement campoNome = driver.findElement(By.xpath("//*[@id=\"full-name\"]"));
+        campoNome.sendKeys("Teste de Software");
+        WebElement campoEmail = driver.findElement(By.xpath("//*[@id=\"email-address\"]"));
+        campoEmail.sendKeys("softwareutfpr@gmail.com");
+        WebElement campoTopic = driver.findElement(By.xpath("//*[@id=\"topic\"]"));
+        campoTopic.sendKeys("Teste com Selenium");
+        WebElement campoMessage = driver.findElement(By.xpath("//*[@id=\"message\"]"));
+        campoMessage.sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a aliquet augue. Suspendisse potenti.");
+        
+        takeScreenShot();
+        
+        WebElement botaoSendMessage = driver.findElement(By.xpath("//*[@id=\"contact\"]/div/div/form/div[5]/button"));
+        botaoSendMessage.click();
+        
+        WebDriverWait wait2 = new WebDriverWait(driver, 10);
+        wait2.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"contact\"]/div/div/p[1]")).
+                                                                    getText().toLowerCase().startsWith("thanks") );
+        takeScreenShot();
+    }
+    
+    private void takeScreenShot() {
+        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            scId++;
+            FileUtils.copyFile(sourceFile, new File("./res/" + scId + ".png"));
+        } catch(IOException e) {}
+    }
+    
+    private void fazLogin(){
         driver.get("https://ration.io/");
         WebElement botaoLogin = driver.findElement(By.xpath("//*[@id=\"page-header\"]/div/a[2]"));
         botaoLogin.click();
@@ -59,47 +111,5 @@ public class ExemploTest {
         campoSenha.sendKeys("teste");
         WebElement campoSignIn = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[4]/button"));
         campoSignIn.click();
-    }
-    
-    @After
-    public void after() {
-        driver.close();
-    }
-    /*
-    @Test
-    public void testGoogleSearch() {
-        driver.get("https://www.google.com.br/");
-        WebElement searchInput = driver.findElement(By.name("q"));
-        
-        searchInput.sendKeys("teste de software");
-        
-        takeScreenShot();
-        
-        searchInput.submit();
-        
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.getTitle().toLowerCase().startsWith("teste") );
-        
-        takeScreenShot();
-        
-        assertTrue(driver.getTitle().startsWith("teste de software"));
-    }
-    */
-    @Test
-    public void testeLogin(){
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.findElement(By.xpath("//*[@id=\"friends\"]/div/div[1]/h1")).
-                                                                    getText().toLowerCase().startsWith("friends") );
-        
-        
-        takeScreenShot();
-    }
-    
-    private void takeScreenShot() {
-        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        try {
-            scId++;
-            FileUtils.copyFile(sourceFile, new File("./res/" + scId + ".png"));
-        } catch(IOException e) {}
     }
 }
