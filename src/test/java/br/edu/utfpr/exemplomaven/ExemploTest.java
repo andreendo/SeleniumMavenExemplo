a0d04c50b1c7d6c52a61a9c83c8a5e1d2c9438f7
rafaelsampy <rafaelsampy@hotmail.com>
2018-09-12 21:05:44 -0300

    testes realizados

--- src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
+++ src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
@@ -26,55 +26,68 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * @author andreendo
  */
 public class ExemploTest {
-
-    private static int scId = 0;
-
-    WebDriver driver;
-    
-    @BeforeClass
-    public static void beforeClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-    
-    @Before
-    public void before() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("headless");
-        chromeOptions.addArguments("window-size=1200x600");
-        chromeOptions.addArguments("start-maximized");
-        
-        driver = new ChromeDriver(chromeOptions);
+    @Test
+    public void testSingUpRation() {
+        System.setProperty("webdriver.chrome.driver", "/home/utfpr/install/chromedriver");
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-    
-    @After
-    public void after() {
-        driver.close();
+        driver.get("https://ration.io/");
+        
+        // Button SignUp
+        WebElement SignUpButton = driver.findElement( By.xpath("//*[@id=\"page-header\"]/div/div/a") );
+        SignUpButton.click();
+        
+        // Input Full Name
+        WebElement FullNameInput = driver.findElement( By.xpath("//*[@id=\"full-name\"]") );
+        FullNameInput.sendKeys("Rafael Hideki de Macedo Sampy");
+
+        // Input E-mail Address
+        WebElement EmailInput = driver.findElement( By.xpath("//*[@id=\"email-address\"]") );
+        EmailInput.sendKeys("rafaelsampy@hotmail.com");
+
+        // Input Choose Password
+        WebElement ChoosePasswordInput = driver.findElement( By.xpath("//*[@id=\"password\"]") );
+        ChoosePasswordInput.sendKeys("teste1234");
+
+        // Input Confirm Password
+        WebElement ConfirmPasswordInput = driver.findElement( By.xpath("//*[@id=\"confirm-password\"]") );
+        ConfirmPasswordInput.sendKeys("teste1234");
+
+        // CheckBox Agree Terms
+        WebElement AgreeTermsCheckBox = driver.findElement( By.xpath("//*[@id=\"terms-agreement\"]") );
+        AgreeTermsCheckBox.isSelected();
+
+        // Button Create Account
+        WebElement CreateAccountButton = driver.findElement( By.xpath("//*[@id=\"signup\"]/div/div/form/div[6]/button/span") );
+        CreateAccountButton.click();
+
     }
     
     @Test
-    public void testGoogleSearch() {
-        driver.get("https://www.google.com.br/");
-        WebElement searchInput = driver.findElement(By.name("q"));
-        searchInput.sendKeys("teste de software");
+    public void testLogInRation() {
+        System.setProperty("webdriver.chrome.driver", "/home/utfpr/install/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://ration.io/");
         
-        takeScreenShot();
+        // Button LogIn
+        WebElement LogInButton = driver.findElement( By.xpath("//*[@id=\"page-header\"]/div/a[2]") );
+        LogInButton.click();
         
-        searchInput.submit();
+        // Input Email Address
+        WebElement EmailAddressInput = driver.findElement( By.xpath("//*[@id=\"login\"]/div/div/form/div[1]/input") );
+        EmailAddressInput.sendKeys("rafaelsampy@hotmail.com");
+
+        // Input Password
+        WebElement InputPasswordInput = driver.findElement( By.xpath("//*[@id=\"login\"]/div/div/form/div[2]/input") );
+        InputPasswordInput.sendKeys("teste1234");
+
+        // CheckBox Remenber Me
+        WebElement RemenberMeCheckBox = driver.findElement( By.xpath("//*[@id=\"remember\"]") );
+        RemenberMeCheckBox.isSelected();
         
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.getTitle().toLowerCase().startsWith("teste") );
-        
-        takeScreenShot();
-        
-        assertTrue(driver.getTitle().startsWith("teste de software"));
-    }
-    
-    private void takeScreenShot() {
-        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        try {
-            scId++;
-            FileUtils.copyFile(sourceFile, new File("./res/" + scId + ".png"));
-        } catch(IOException e) {}
+        // Button Sign In
+        WebElement SignInButton = driver.findElement( By.xpath("//*[@id=\"login\"]/div/div/form/div[4]/button") );
+        SignInButton.click();
     }
 }
